fd8cc094a73c9316f6c87f05645ee7488a581375
Joel Kaasinen <joel.kaasinen@gmail.com>
2020-11-23 18:59:11 +0200

    Set 7 Ex 3: test Error->Error state transition
    closes #15

--- exercises/Set7Test.hs
+++ exercises/Set7Test.hs
@@ -88,7 +88,8 @@ genBad = do
   i <- choose (0,length g - 1)
   let pre = take i g
   x <- arbitrary `suchThat` (\x -> not (canFinish (pre++[x]) || finished (pre++[x])))
-  return $ pre++[x]
+  y <- arbitrary
+  elements [pre++[x], pre++[x,y]]
 
 ex3_error = forAllBlind (genBad) $ \events ->
   $(testing [|bake events|]) (?==Error)
