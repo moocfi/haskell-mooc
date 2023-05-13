76d7473484747f9083ed395abfa37c69a5bc405a
xikass <mohammad.a.zachariah@gmail.com>
2023-05-13 14:08:51 +0300

    start Set4a

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -35,7 +35,8 @@ import Data.Array
 -- you remove the Eq a => constraint from the type!
 
 allEqual :: Eq a => [a] -> Bool
-allEqual xs = todo
+allEqual [] = True
+allEqual [x] = True
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function distinct which returns True if all
