950432512f62002611a9f86df9bc8a70e6eac6e9
Milko Poolstok <milko.burkard1997@gmail.com>
2024-06-25 17:36:08 +0200

    Solved 13

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -260,8 +260,8 @@ multiCompose fs = head fs . (multiCompose $ drop 1 fs)
 --   multiApp id [head, (!!2), last] "axbxc" ==> ['a','b','c'] i.e. "abc"
 --   multiApp sum [head, (!!2), last] [1,9,2,9,3] ==> 6
 
--- multiApp :: ([a] -> b) -> [a -> a] -> (a -> b)
-multiApp = todo
+multiApp :: ([b] -> c) -> [a -> b] -> a -> c
+multiApp f gs x = f $ [g x | g <- gs]
 
 ------------------------------------------------------------------------------
 -- Ex 14: in this exercise you get to implement an interpreter for a
